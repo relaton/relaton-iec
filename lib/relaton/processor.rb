4b440cb89624f32b365bb8d1f6699e38439f3571
Nick Nicholas <opoudjis@optusnet.com.au>
2018-09-19 18:25:54 +1000

    relaton

--- lib/relaton/processor.rb
+++ lib/relaton/processor.rb
@@ -5,14 +5,14 @@ module Relaton
     class Processor < Relaton::Processor
 
       def initialize
-        @short = :isobib
+        @short = :iecbib
         @prefix = "IEC"
         @defaultprefix = %r{^(IEC)[ /]|^IEV($| )}
         @idtype = "IEC"
       end
 
       def get(code, date, opts)
-        ::Isobib::IsoBibliography.get(code, date, opts)
+        ::Iecbib::IecBibliography.get(code, date, opts)
       end
     end
   end
